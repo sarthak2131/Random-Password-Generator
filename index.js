37ed39ab382ef66877fdcad2e17c5d5ae0db00b5
sarthak2131 <sarthakbaderiya19@gmail.com>
2025-02-04 00:12:08 +0530

    add slider functionality to update password length display

--- index.js
+++ index.js
@@ -1,7 +1,14 @@
 document.addEventListener("DOMContentLoaded", () => {
     const btn = document.getElementById('generateBtn');
     const newPass = document.getElementById('newPass');
+    const slider = document.getElementById('lengthRange');
+    const rangeVal = document.getElementById('rangeVal')
+    
 
+    slider.addEventListener('input',(e)=>{
+        rangeVal.innerText = e.target.value
+
+    })
     btn.addEventListener('click', () => {
         newPass.value = "New Password Generated"; 
     });
