95498c35029352f9de9310a6badf510c047faa49
sarthak2131 <sarthakbaderiya19@gmail.com>
2025-02-08 18:50:11 +0530

    add functionality to include character set options and copy password to clipboard

--- index.js
+++ index.js
@@ -3,8 +3,11 @@ document.addEventListener("DOMContentLoaded", () => {
     const newPass = document.getElementById('newPass');
     const slider = document.getElementById('lengthRange');
     const rangeVal = document.getElementById('rangeVal')
-    
-
+    const upperCase = document.getElementById('uppercase')
+    const lowerCase = document.getElementById('lowercase')
+    const numbers = document.getElementById('numbers')
+    const symbols = document.getElementById('symbols')
+    const copybtn = document.getElementById('copyBtn')
     slider.addEventListener('input',(e)=>{
         rangeVal.innerText = e.target.value
 
@@ -17,7 +20,26 @@ document.addEventListener("DOMContentLoaded", () => {
     let smallLetters = 'abcdefghijklmnopqrstuvwxyz';
     let numberStr = '0123456789';
     let symbolsStr = '!@#$%^&*()_+[]{}|;:,.<>?';
-    let finalStr = capitalLetters
+    let finalStr = ''
+
+        if(upperCase.checked){
+          finalStr += capitalLetters;
+        }
+        if(lowerCase.checked){
+            finalStr+=smallLetters
+        }
+        if(numbers.checked){
+            finalStr+=numberStr
+        }
+        if(symbols.checked){
+            finalStr+=symbolsStr
+        }
+        if(finalStr === ''){
+            alert("Please select at least one field.");
+            newPass.value = "";
+            return; 
+        }
+        console.log(finalStr)
     let latestPass = ''
     for (let i = 0; i < slider.value; i++) {
         let randNum = Math.floor(Math.random()*finalStr.length )
@@ -25,4 +47,7 @@ document.addEventListener("DOMContentLoaded", () => {
     }
     newPass.value = `${latestPass}`
     });
+    copybtn.addEventListener('click',()=>{
+       window.navigator.clipboard.writeText(newPass.value)
+    })
 });
